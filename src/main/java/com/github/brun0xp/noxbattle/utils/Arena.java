3bb2e95c0dd8428cc11cb156eaee385da678058b
Brun0XP <brunosilvarodrigues2304@gmail.com>
2019-03-20 23:00:22 -0300

    Add Object Battle

---
+++ src/main/java/com/github/brun0xp/noxbattle/utils/Arena.java
@@ -0,0 +1,4 @@
+package com.github.brun0xp.noxbattle.utils;
+
+public class Arena {
+}
---
+++ src/main/java/com/github/brun0xp/noxbattle/utils/Battle.java
@@ -0,0 +1,9 @@
+package com.github.brun0xp.noxbattle.commands;
+
+import org.bukkit.entity.Player;
+
+public class Battle {
+
+    private Player challenger;
+    private Player challenged;
+}
